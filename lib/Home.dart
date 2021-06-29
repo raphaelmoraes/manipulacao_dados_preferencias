f627fd4cfc2b493617bb70719868f829f6f6009f
Raphael <viladapenha@gmail.com>
2021-06-29 10:08:45 -0300

    Manipulação de dados - Preferencias

--- lib/Home.dart
+++ lib/Home.dart
@@ -1,52 +1,48 @@
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-//Não consegui atualizar na tela
-
 class Home extends StatefulWidget {
-  const Home({Key? key}) : super(key: key);
-
   @override
   _HomeState createState() => _HomeState();
 }
 
 class _HomeState extends State<Home> {
+  TextEditingController _controllerCampo = TextEditingController();
+
+  var _textSalvo = "Nada salvo";
+
+  _salvar() async {
+    String valorDigitado = _controllerCampo.text;
+
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setString("nome", valorDigitado);
+
+    setState(() {
+      _textSalvo = prefs.getString("nome") ?? "Sem valor";
+    });
+
+    print("salvar - $valorDigitado");
+  }
+
+  void _recuperar() async {
+    final prefs = await SharedPreferences.getInstance();
+
+    setState(() {
+      _textSalvo = prefs.getString("nome") ?? "Sem valor";
+    });
+
+    print("recuperar - $_textSalvo");
+  }
+
+  void _remover() async {
+    final prefs = await SharedPreferences.getInstance();
+
+    prefs.remove("nome");
+    print("remover");
+  }
+
   @override
   Widget build(BuildContext context) {
-    TextEditingController _controllerCampo = TextEditingController();
-
-    String _textSalvo = "Nada salvo";
-
-    _salvar() async {
-      String valorDigitado = _controllerCampo.text;
-
-      final prefs = await SharedPreferences.getInstance();
-      await prefs.setString("nome", valorDigitado);
-
-      setState(() {
-        _textSalvo = prefs.getString("nome") ?? "Sem valor 1";
-      });
-
-      print("salvar - $valorDigitado");
-    }
-
-    _recuperar() async {
-      final prefs = await SharedPreferences.getInstance();
-
-      setState(() {
-        _textSalvo = prefs.getString("nome") ?? "Sem valor 1";
-      });
-
-      print("recuperar - $_textSalvo");
-    }
-
-    _remover() async {
-      final prefs = await SharedPreferences.getInstance();
-
-      prefs.remove("nome");
-      print("remover");
-    }
-
     return Scaffold(
       appBar: AppBar(
         title: Text("Manipulação de dados"),
